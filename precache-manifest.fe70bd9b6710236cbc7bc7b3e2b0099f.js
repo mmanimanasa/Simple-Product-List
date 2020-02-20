91a42c764a9a335c5bf2b2e0111a88df82c8c2e8
Manasa Mandadapu <m.manasa.mandadapu@accenture.com>
2020-02-20 11:41:22 +0530

    Updates

---
+++ precache-manifest.fe70bd9b6710236cbc7bc7b3e2b0099f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa897d67a46a10da993b22fcf013a648",
+    "url": "/React-productlist/index.html"
+  },
+  {
+    "revision": "d8d0bf3132f4092bbe6e",
+    "url": "/React-productlist/static/css/main.a22ed1f7.chunk.css"
+  },
+  {
+    "revision": "6fbc3ca1afc459ec6d7d",
+    "url": "/React-productlist/static/js/2.6c2c468a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-productlist/static/js/2.6c2c468a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8d0bf3132f4092bbe6e",
+    "url": "/React-productlist/static/js/main.a0ec9b4d.chunk.js"
+  },
+  {
+    "revision": "744fa0aeb03427f47f7d",
+    "url": "/React-productlist/static/js/runtime-main.81198242.js"
+  }
+]);
